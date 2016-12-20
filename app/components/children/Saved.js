53ec1072e8d7d2679bdb698ac8e1e99a55cc3d44
Genevieve Conty <gmireillec@Genevieves-MacBook-Pro.local>
2016-12-20 12:17:59 -0500

    incorporating helpers1

--- app/components/children/Saved.js
+++ app/components/children/Saved.js
@@ -1,27 +1,42 @@
+// TODO: 
+// - delete button
+// - move to own page 
+//		- app/pinnedArticles --> render(whatever)
+
 // Include React
 var React = require("react");
 
-// Creating the Results component
+// Helper for making AJAX requests to our API
+var helpers = require("../utils/helpers");
+
+// Creating the saved articles component
 var Saved = React.createClass({
-  // Here we render the function
-  render: function() {
-    return (
-    	<div className="saved-entry">
-    		<div className="results col s12">
-	    		<h2>Saved Articles</h2>
-			    <ul className="collection">
-			        <li className="collection-item avatar">
-						<i className="material-icons circle green">folder</i>
-						<span className="title">Article Title</span>
-						<br/><span>Date Line</span>
-						<a className="waves-effect waves-light btn"><i className="material-icons left">no_sim</i>Delete</a>
-						<a className="waves-effect waves-light btn"><i className="material-icons left">subject</i>View</a>
-					</li>
-			    </ul>
-    		</div>
-    	</div>
-    );
-  }
+	// render saved articles on load
+	componentDidMount: function(){
+		helpers.getSaved()
+			.then(function(response)){
+				this.setState({ savedArticles: response.data})
+			}.bind(this);
+	},
+	// Here we render the function
+	render: function() {
+	    return (
+	    	<div className="saved-entry">
+	    		<div className="results col s12">
+		    		<h2>Saved Articles</h2>
+				    <ul className="collection">
+				        <li className="collection-item avatar">
+							<i className="material-icons circle green">folder</i>
+							<span className="title">Article Title</span>
+							<br/><span>Date Line</span>
+							<a className="waves-effect waves-light btn"><i className="material-icons left">no_sim</i>Delete</a>
+							<a className="waves-effect waves-light btn"><i className="material-icons left">subject</i>View</a>
+						</li>
+				    </ul>
+	    		</div>
+	    	</div>
+	    );
+	}
 });
 
 // Export the component back for use in other files
--- app/components/children/Search.js
+++ app/components/children/Search.js
@@ -5,31 +5,38 @@ var Form = require("./grandchildren/Form.js");
 var Results = require("./grandchildren/Results.js");
 
 // Helper for making AJAX requests to our API
-var helpers = require("./utils/helpers");
+var helpers = require("../utils/helpers");
 
 // Creating the Results component
 var Search = React.createClass({
 	getInitialState: function(){
-		return { title: "", startDate: "", endDate:""};
+		return { searchResults: ""};
 	},
+	//get serch terms from form
 	setSearchTerms: function(title, date, url){
 		this.setState({ 
 			title: title,
 			date: date,
 			url: url
 		})
+	},	
+	// update results component when search entered and get results
+	componentDidUpdate: function(){
+		//run nyt query with search terms
+		helpers.runQuery(this.state.title, this.state.date, this.state.url)
+			.then(function(data){
+				console.log("Query ran", data);
+				this.setState({ searchResults: data});
+			});
+		}.bind(this);
 	},
-	// render saved articles on load
-
-	// update when search entered
-
-
 	// Here we render the function
 	render: function() {
 		return (
 			<div className="row">
-				<Form />
-				<Results />
+				{/* pass function to form as prop; pass searchResults to component as state*/}
+				<Form setSearchTerms = {this.setSearchTerms}/>
+				<Results results={this.state.results}/>
 			</div>
 		);
 	}
